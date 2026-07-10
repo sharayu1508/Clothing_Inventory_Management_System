97a8493fe345204f1876189825ae72ba55d7f9a8
riddhijadhav656-stack <riddhijadhav656@gmail.com>
2026-07-10 22:17:15 +0530

    Completed module

--- Backend/database.py
+++ Backend/database.py
@@ -1,17 +1,25 @@
 import mysql.connector
-
-
 def get_connection():
     try:
         connection = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="your_mysql_password",
+            password="RIDDHI@134",
             database="clothing_inventory"
         )
-
+        if connection.is_connected():
+            print("Database Connected Successfully!")
         return connection
 
     except mysql.connector.Error as err:
         print("Database Connection Error:", err)
         return None
+        return None
+    
+if __name__=="__main__":
+    conn = get_connection()
+
+    if conn:
+        print("Database connected Successfully!")
+    else:
+        print("Database connection Failed!")
