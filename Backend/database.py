fece49c3a2149284dffd4a272e1f82b6e6ea0b04
Sharayu Jadhav <sharayurjadhav@gmail.com>
2026-07-10 22:56:12 +0530

    database connection module

--- Backend/database.py
+++ Backend/database.py
@@ -6,7 +6,7 @@ def get_connection():
         connection = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="your_mysql_password",
+            password="sharayu15",
             database="clothing_inventory"
         )
 
